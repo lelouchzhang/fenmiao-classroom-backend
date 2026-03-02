b81538a0c79f58b57ad4c926c4f96a42e8d2f351
lelouchZhang <zhch8128@gmail.com>
2026-03-03 00:37:32 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 
 import { auth } from "./lib/auth.js";
 import { toNodeHandler } from "better-auth/node";
-import securityMiddleware from "./middleware/security.js";
+// import securityMiddleware from "./middleware/security.js";
 import subjectsRouter from "./routes/subjects.js";
 import usersRouter from "./routes/users.js";
 import classesRouter from "./routes/classes.js";
@@ -24,16 +24,20 @@ app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 
-app.use(securityMiddleware);
+// app.use(securityMiddleware);
 
 app.get("/", (req, res) => res.send("Hello World"));
 
 app.use("/api/subjects", subjectsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/classes", classesRouter);
+app.use("/api/departments");
+app.use("/api/stats");
+app.use("/api/enrollments");
 
 app.set("trust proxy", true);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`);
+  // console.log(`Server running on port http://localhost:${PORT}`);
+  console.log(`...Classroom后端已启动`);
 });
